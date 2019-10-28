4b039ffa0d26c35f3ba00217df736c8205799b0c
sammydolgin <52990412+sammydolgin@users.noreply.github.com>
2019-10-28 11:33:27 -0400

    Update global.R
    Cleaned up code, added comments

--- global.R
+++ global.R
@@ -4,25 +4,33 @@ library(dplyr)
 library(ggplot2)
 library(readr)
 library(stringr)
+library(forcats)
+
+#-----Importing the dataframe-----#
 
 df <- read_csv("Education.csv")
+
+#-----String manipulation within variable names-----#
 
 newcols = tolower(names(df))
 newcols = str_replace_all(string=newcols, pattern=" ", repl="_")
 names(df) = newcols
 colnames(df)[colnames(df)=="strong_family-community_ties_rating"] = "strong_family_community_ties_rating"
-df = filter(
-  df, rigorous_instruction_rating!="N/A",
-  collaborative_teachers_rating!="N/A",
-  supportive_environment_rating!="N/A",
-  effective_school_leadership_rating!="N/A",
-  strong_family_community_ties_rating!="N/A",
-  trust_rating!="N/A"
-)
+
+#-----Converting string data to numeric values-----#
+
 df$average_ela_proficiency = as.numeric(df$average_ela_proficiency)
 df$average_math_proficiency = as.numeric(df$average_math_proficiency)
+df$economic_need_index = as.numeric(df$economic_need_index)
+df$student_attendance_rate = as.numeric(str_replace(df$student_attendance_rate, "%", ""))
+
+#-----Removing extraneous columns-----#
+
 df = df[1:41]
 df = df[-1:-3]
+
+#-----Creating the racial_majority variable-----#
+
 asian_vector = ((df$percent_asian > df$percent_black) & (df$percent_asian > df$percent_hispanic) & (df$percent_asian > df$percent_white))
 black_vector = ((df$percent_black > df$percent_asian) & (df$percent_black > df$percent_hispanic) & (df$percent_black > df$percent_white))
 hispanic_vector = ((df$percent_hispanic > df$percent_asian) & (df$percent_hispanic > df$percent_black) & (df$percent_hispanic > df$percent_white))
@@ -32,7 +40,7 @@ df$predominantly_asian = asian_vector
 df$predominantly_black = black_vector
 df$predominantly_hispanic = hispanic_vector
 df$predominantly_white = white_vector
-df$racial_majority = ""
+df$racial_majority = " "
 
 for (i in c(1:length(df$racial_majority))) {
   if (df$predominantly_asian[i]) {
@@ -45,11 +53,28 @@ for (i in c(1:length(df$racial_majority))) {
     df$racial_majority[i] = "White"
   }}
 
-df = filter(df, racial_majority!="Other", racial_majority!="")
-df$economic_need_index = as.numeric(df$economic_need_index)
+#-----Removing N/As, blank values, and outliers-----#
 
-df$student_attendance_rate = as.numeric(str_replace(df$student_attendance_rate, "%", ""))
-gd = df %>% group_by(racial_majority) %>% summarise(economic_need_index = mean(economic_need_index), average_ela_proficiency = mean(average_ela_proficiency))
+df = filter(
+  df, rigorous_instruction_rating!="N/A",
+  collaborative_teachers_rating!="N/A",
+  supportive_environment_rating!="N/A",
+  effective_school_leadership_rating!="N/A",
+  strong_family_community_ties_rating!="N/A",
+  trust_rating!="N/A"
+)
+
+df = filter(df, racial_majority!="Other", racial_majority!=" ", student_attendance_rate>70)
+
+#-----Creating the group_by to plot mean values-----#
+
+gd = df %>% group_by(racial_majority) %>% 
+  summarise(economic_need_index = mean(economic_need_index),
+            average_ela_proficiency = mean(average_ela_proficiency),
+            average_math_proficiency = mean(average_math_proficiency),
+            student_attendance_rate = mean(student_attendance_rate))
+
+#-----Creating value hierarchy within Support Metrics to allow for an ordered legend-----#
 
 df$rigorous_instruction_rating = factor(df$rigorous_instruction_rating, levels = c("Not Meeting Target", "Approaching Target", "Meeting Target", "Exceeding Target"))
 df$collaborative_teachers_rating = factor(df$collaborative_teachers_rating, levels = c("Not Meeting Target", "Approaching Target", "Meeting Target", "Exceeding Target"))
@@ -58,6 +83,7 @@ df$effective_school_leadership_rating = factor(df$effective_school_leadership_ra
 df$strong_family_community_ties_rating = factor(df$strong_family_community_ties_rating, levels = c("Not Meeting Target", "Approaching Target", "Meeting Target", "Exceeding Target"))
 df$trust_rating = factor(df$trust_rating, levels = c("Not Meeting Target", "Approaching Target", "Meeting Target", "Exceeding Target"))
 
+#-----Creating string vectors to be used within our SelectizeInput selectors-----#
 
 cols = c(names(df)[25], names(df)[27], names(df)[29], names(df)[31], names(df)[33], names(df)[35])
 subj = c(names(df)[37], names(df)[38])
